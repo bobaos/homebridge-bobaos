c478a75292df732d10f8bbf85ecf06f7ba407993
Vladimir Shabunin <va.shabunin@physics.msu.ru>
2018-04-23 20:25:02 +0300

    Second commit

---
+++ yml2configjson/genBdsdPlatformConfig.js
@@ -0,0 +1,54 @@
+// TODO:> get config.json as first commandline arg
+// TODO:> get yml file as second arg
+// TODO:> generate json and stdout it
+
+// Usage: node genBdsdPlatformConfig yamlFile.yml config.json "Platform"
+const yaml = require('js-yaml');
+const fs = require('fs');
+
+if (process.argv.length < 3) {
+  throw new Error('Please provide yaml file as a commandline argument.');
+}
+
+if (process.argv.length < 4) {
+  throw new Error('Please provide config.json file as a commandline argument.')
+}
+
+// now platform name
+let platformName = "Bobaos";
+if (process.argv[4]) {
+  platformName = process.argv[4];
+}
+
+let yamlFilePath = process.argv[2];
+let configFilePath = process.argv[3];
+
+
+try {
+  let ymlDoc = yaml.safeLoad(fs.readFileSync(yamlFilePath, 'utf8'));
+  let jsonDoc = JSON.parse(fs.readFileSync(configFilePath, 'utf8'));
+  // TODO: let platforms
+  if (jsonDoc.platforms !== undefined && Array.isArray(jsonDoc.platforms)) {
+    const findByPlatform = t => {
+      return t.platform === platformName
+    };
+    let platformIndex = jsonDoc.platforms.findIndex(findByPlatform);
+    if (platformIndex > -1) {
+      jsonDoc.platforms[platformIndex].accessories = ymlDoc.accessories;
+      console.log(JSON.stringify(jsonDoc,' ', 2));
+      process.exit();
+    } else {
+      // TODO: push
+      let newPlatform = {
+        platform: platformName,
+        name: platformName.toLowerCase(),
+        accessories: ymlDoc.accessories
+      };
+      jsonDoc.platforms.push(newPlatform);
+      console.log(JSON.stringify(jsonDoc, ' ', 2));
+      process.exit();
+    }
+  }
+} catch (e) {
+  console.log(e);
+}
