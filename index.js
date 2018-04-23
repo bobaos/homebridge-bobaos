482401791075a064fffb565115b04c791ea67715
Vladimir Shabunin <va.shabunin@physics.msu.ru>
2018-04-23 20:23:48 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const BdsdClient = require('bdsd.client');
+const BobaosAccessory = require('./lib/accessory');
+
+let Service, Characteristic;
+let myBobaos;
+
+module.exports = function (homebridge) {
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  homebridge.registerPlatform("homebridge-bdsd", "Bobaos", BobaosPlatform);
+};
+
+function BobaosPlatform(log, config) {
+  this._config = {};
+  this.log = log;
+  if (Object.prototype.hasOwnProperty.call(config, 'sockfile')) {
+    myBobaos = new BdsdClient(config['sockfile']);
+  } else {
+    myBobaos = new BdsdClient();
+  }
+  if (Object.prototype.hasOwnProperty.call(config, 'accessories')) {
+    this._config.accessories = config['accessories'].slice();
+  }
+}
+
+BobaosPlatform.prototype = {
+  accessories: function (cb) {
+    let foundAccessories = [];
+    // TODO: _config.accessories.forEach => create new BobaosAccessory
+    // TODO: foundAccessories.push, cb(foundAccessories);
+    let params = {
+      Service: Service,
+      Characteristic: Characteristic,
+      Bobaos: myBobaos,
+      log: this.log
+    };
+    this._config.accessories.forEach(a => {
+      let newAccessory = new BobaosAccessory(Object.assign({accessory: a}, params));
+      foundAccessories.push(newAccessory);
+    });
+    cb(foundAccessories);
+  }
+};
